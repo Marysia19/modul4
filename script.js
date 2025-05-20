2673946bc3629791c44be4e4aa0d2b1c0bf82cfa
Marysia19 <marykozlova2005@mail.ru>
2025-05-20 19:01:19 +0300

    first

--- script.js
+++
@@ -1,7 +0,0 @@
-
-const shapes = document.querySelectorAll('.shapes > div');
-setInterval(() => {
-    shapes.forEach(shape => {
-        shape.style.transform = `translate(${Math.random() * 100 - 50}px, ${Math.random() * 100 - 50}px) rotate(${Math.random() * 360}deg)`;
-    });
-}, 4000);
